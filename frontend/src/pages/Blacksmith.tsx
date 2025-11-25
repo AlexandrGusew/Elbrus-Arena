6e4e9fbd577cdf5b64c793fc453b88d079408e0c
AlexandrGusew <alexandr.gu@tutamail.com>
2025-11-25 20:06:12 +0300

    fix13

--- frontend/src/pages/Blacksmith.tsx
+++ frontend/src/pages/Blacksmith.tsx
@@ -2,19 +2,6 @@ import { useCharacter } from '../hooks/useCharacter';
 
 const Blacksmith = () => {
   const { character, loading } = useCharacter();
-    if (!character) return;
-
-    try {
-      const endpoint = invItem.isEquipped
-        ? `/character/${character.id}/unequip/${invItem.id}`
-        : `/character/${character.id}/equip/${invItem.id}`;
-
-      const { data: updatedCharacter } = await api.put<Character>(endpoint);
-
-      setCharacter(updatedCharacter);
-    } catch (err: any) {
-    }
-  };
 
   if (loading) {
     return <div style={{ padding: '20px', textAlign: 'center' }}>Загрузка...</div>;
