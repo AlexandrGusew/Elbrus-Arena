23485d3c5cf7aff2dac01eea311d00bc41726fe5
AlexandrGusew <alexandr.gu@tutamail.com>
2025-11-29 21:59:28 +0300

    правки

--- backend/src/pvp/pvp.module.ts
+++ backend/src/pvp/pvp.module.ts
@@ -3,9 +3,10 @@ import { PvpService } from './pvp.service';
 import { PvpController } from './pvp.controller';
 import { PvpGateway } from './pvp.gateway';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ChatModule],
   controllers: [PvpController],
   providers: [PvpService, PvpGateway],
   exports: [PvpService],
